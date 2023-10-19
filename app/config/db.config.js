566c6aef8fe34fa496af95adc02824805df9bd01
chunyim <chunhinyim@gmail.com>
2023-10-19 15:10:26 -0400

    added env variable

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,3 +1,7 @@
+// module.exports = {
+//     url:"mongodb+srv://chunhinhenry:VNpx7wuMKfld83tF@problemdata.ltipdih.mongodb.net/problemlist?retryWrites=true&w=majority"
+//     };
+
 module.exports = {
-    url:"mongodb+srv://chunhinhenry:VNpx7wuMKfld83tF@problemdata.ltipdih.mongodb.net/problemlist?retryWrites=true&w=majority"
+    url: process.env.MONGODB_URI
     };
